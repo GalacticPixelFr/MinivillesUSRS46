af79cf9efc467a1c6110501d612eca847fab3f37
Didios <78910620+Didios@users.noreply.github.com>
2021-10-25 11:18:32 +0200

    pile de carte
    ajout de la pile de carte

--- MinivillesURSR46/MinivillesURSR46/Piles.cs
+++ MinivillesURSR46/MinivillesURSR46/Piles.cs
@@ -1,7 +1,83 @@
-﻿namespace MinivillesURSR46
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinivillesURSR46
 {
     public class Piles
     {
-        
+        public List<CardsInfo> pile;
+        private Random rnd = new Random();
+
+        public Piles()
+        {
+            pile = new List<CardsInfo>();
+        }
+        public Piles(List<Cardsinfo> liste)
+        {
+            pile = liste;
+        }
+
+        /// <summary>
+        /// Méthode permettant de mélanger les cartes du paquet
+        /// </summary>
+        public void Shuffle()
+        {
+            pile = pile.OrderBy(item => rnd.Next()).ToList();
+        }
+
+        /// <summary>
+        /// Méthode permettant d'ajouter une carte dans la pile
+        /// place la carte en dernière position
+        /// </summary>
+        /// <param name="c">carte a ajouté à la pile</param>
+        public void AddCard(CardsInfo c)
+        {
+            pile.Add(c);
+        }
+
+        /// <summary>
+        /// Méthode permettant d'obtenir la première cartes du paquet/de la pile
+        /// </summary>
+        /// <returns>Renvoi la carte en première position de la pile</returns>
+        public CardsInfo GetCards()
+        {
+            // vérification pile non vide
+            if (pile.Count == 0)
+                return null;
+
+            CardsInfo c = pile[0];
+            pile.RemoveAt(0);
+            return c;
+
+        }
+        /// <summary>
+        /// Surcharge permettant d'obtenir la première carte avec l'ID souhaité
+        /// </summary>
+        /// <param name="ID">un entier étant l'ID de la carte souhaité</param>
+        /// <returns>Renvoi la carte en première position de la pile</returns>
+        public CardsInfo GetCard(int ID)
+        {
+            // on vérifie que la carte recherché existe
+            int index = -1;
+
+            for (int i = 0; i < pile.Count; i++)
+            {
+                if (pile[i].ID == ID)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            // s'il n'y a pas de cartes comme celle recherché
+            if (index == -1)
+                return null;
+
+            CardsInfo = pile[index];
+            pile.RemoveAt(index);
+            return c;
+        }
     }
 }

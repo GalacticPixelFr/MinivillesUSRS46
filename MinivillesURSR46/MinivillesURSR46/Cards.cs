828de30c8c6701ec78422d256573dad4f54101b9
GalacticPixelFr <contact@galactic-pixel.com>
2021-10-25 14:49:43 +0200

    Player complete + rework color CardsInfo

--- MinivillesURSR46/MinivillesURSR46/Cards.cs
+++ MinivillesURSR46/MinivillesURSR46/Cards.cs
@@ -22,42 +22,42 @@ namespace MinivillesURSR46
 
         public CardsInfo CreateChampDeBle()
         {
-            return new CardsInfo(0, "bleu", 1, "Champ de Blé", "Recevez 1 pièce", 1, 1);
+            return new CardsInfo(0, Color.Bleu, 1, "Champ de Blé", "Recevez 1 pièce", 1, 1);
         }
         
         public CardsInfo CreateFerme()
         {
-            return new CardsInfo(1, "bleu", 2, "Ferme", "Recevez 1 pièce", 1, 1);
+            return new CardsInfo(1, Color.Bleu, 2, "Ferme", "Recevez 1 pièce", 1, 1);
         }
         
         public CardsInfo CreateBoulangerie()
         {
-            return new CardsInfo(2, "vert", 1, "Boulangerie", "Recevez 2 pièces", 2, 2);
+            return new CardsInfo(2, Color.Vert, 1, "Boulangerie", "Recevez 2 pièces", 2, 2);
         }
         
         public CardsInfo CreateCafe()
         {
-            return new CardsInfo(3, "rouge", 2, "Café", "Recevez 1 pièce du joueur qui à lancé le dé", 3, 1);
+            return new CardsInfo(3, Color.Rouge, 2, "Café", "Recevez 1 pièce du joueur qui à lancé le dé", 3, 1);
         }
         
         public CardsInfo CreateSuperette()
         {
-            return new CardsInfo(4, "vert", 2, "Superette", "Recevez 3 pièces", 4, 3);
+            return new CardsInfo(4, Color.Vert, 2, "Superette", "Recevez 3 pièces", 4, 3);
         }
 
         public CardsInfo CreateForet()
         {
-            return new CardsInfo(5, "bleu", 2, "Forêt", "Recevez 1 pièces", 5, 1);
+            return new CardsInfo(5, Color.Bleu, 2, "Forêt", "Recevez 1 pièces", 5, 1);
         }
         
         public CardsInfo CreateRestaurant()
         {
-            return new CardsInfo(6, "rouge", 4, "Restaurant", "Recevez 2 pièce du joueur qui à lancé le dé", 5, 2);
+            return new CardsInfo(6, Color.Rouge, 4, "Restaurant", "Recevez 2 pièce du joueur qui à lancé le dé", 5, 2);
         }
         
         public CardsInfo CreateStade()
         {
-            return new CardsInfo(7, "bleu", 6, "Stade", "Recevez 4 pièces", 6, 4);
+            return new CardsInfo(7, Color.Bleu, 6, "Stade", "Recevez 4 pièces", 6, 4);
         }
     }
 }

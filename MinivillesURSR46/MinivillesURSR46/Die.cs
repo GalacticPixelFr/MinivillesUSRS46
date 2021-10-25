e1dcebb77f54a00f61018b93cadc678947439bbc
GalacticPixelFr <contact@galactic-pixel.com>
2021-10-25 11:17:13 +0200

    Update Die.cs

--- MinivillesURSR46/MinivillesURSR46/Die.cs
+++ MinivillesURSR46/MinivillesURSR46/Die.cs
@@ -1,7 +1,21 @@
-﻿namespace MinivillesURSR46
+﻿using System;
+
+namespace MinivillesURSR46
 {
     public class Die
     {
+        private Random random = new Random();
+        public int Face { get; protected set; }
+
+        public Die()
+        {
+            
+        }
         
+        public virtual int Lancer()
+        {
+            Face = random.Next(0, 6) + 1;
+            return Face;
+        }
     }
 }

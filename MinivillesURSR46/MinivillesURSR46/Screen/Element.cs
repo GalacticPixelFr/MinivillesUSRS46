898d7940765dc835122f9105a6749244cbfc5fca
kams <camspzlz@gmail.com>
2021-10-25 11:06:27 +0200

    Ajout animation lettre par lettre

--- MinivillesURSR46/MinivillesURSR46/Screen/Element.cs
+++ MinivillesURSR46/MinivillesURSR46/Screen/Element.cs
@@ -1,60 +1,69 @@
 using System;
 using System.Collections.Generic;
 
-public class Element
+namespace MinivillesURSR46
 {
-    public string[] text;
-    public Coordinates coordinates;
-    public Animation animation;
-    public Placement placement;
-    public ConsoleColor foreground;
-    public ConsoleColor background;
-
-    public Element(Coordinates coordinates, string text)
+    public class Element
     {
-        this.text = new string[1] {text};
-        this.coordinates = coordinates;
+        public string[] text;
+        public Coordinates coordinates;
+        public Animation animation;
+        public Placement placement;
+        public ConsoleColor foreground;
+        public ConsoleColor background;
+        public int[] animationIndex;
 
-        this.animation = Animation.None;
-        this.placement = Placement.topLeft;
-        this.foreground = ConsoleColor.White;
-        this.background = ConsoleColor.Black;
+        public Element(Coordinates coordinates, string text)
+        {
+            this.text = new string[1] {text};
+            this.coordinates = coordinates;
 
+            this.animation = Animation.None;
+            this.placement = Placement.topLeft;
+            this.foreground = ConsoleColor.White;
+            this.background = ConsoleColor.Black;
+
+            this.animationIndex = new int[1]{text.Length};
+
+        }
+
+        public Element(string[] text, Coordinates coordinates, Animation animation, Placement placement, ConsoleColor foreground, ConsoleColor background)
+        {
+            this.text = text;
+            this.coordinates = coordinates;
+            this.animation = animation;
+            this.placement = placement;
+            this.foreground = foreground;
+            this.background = background;
+
+            this.animationIndex = text.AsEnumerable().Select(x => x.Length).ToArray();
+        }
     }
 
-    public Element(string[] text, Coordinates coordinates, Animation animation, Placement placement, ConsoleColor foreground, ConsoleColor background)
+    public enum Animation
     {
-        this.text = text;
-        this.coordinates = coordinates;
-        this.animation = animation;
-        this.placement = placement;
-        this.foreground = foreground;
-        this.background = background;
+        None,
+        LetterByLetter
     }
-}
 
-public enum Animation
-{
-    None,
-}
-
-public enum Placement
-{
-    topLeft,
-    topRight,
-    botLeft,
-    botRight,
-    mid
-}
+    public enum Placement
+    {
+        topLeft,
+        topRight,
+        botLeft,
+        botRight,
+        mid
+    }
 
 
-public class Coordinates
-{
-    public int x;
-    public int y;
+    public class Coordinates
+    {
+        public int x;
+        public int y;
 
-    public Coordinates(int x, int y) {
-        this.x = x;
-        this.y = y;
+        public Coordinates(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
--- MinivillesURSR46/MinivillesURSR46/Screen/Screen.cs
+++ MinivillesURSR46/MinivillesURSR46/Screen/Screen.cs
@@ -2,159 +2,188 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-class Screen
+namespace MinivillesURSR46
 {
-    public int height;
-    public int width;
+    class Screen
+    {
+        public int height;
+        public int width;
 
-    //Dictionary<Coordinates, string[]> elements = new();
-    Dictionary<int, List<Element>> layers = new();
+        //Dictionary<Coordinates, string[]> elements = new();
+        Dictionary<int, List<Element>> layers = new();
 
-    /// <summary>
-    /// Constructeur de la classe, crée une fenêtre carré
-    /// </summary>
-    /// <param name="size">Permet de définir la taille d'un côté de l'écran</param>
-    public Screen(int size) {
-        this.height = size;
-        this.width = size;
-    }
+        /// <summary>
+        /// Constructeur de la classe, crée une fenêtre carré
+        /// </summary>
+        /// <param name="size">Permet de définir la taille d'un côté de l'écran</param>
+        public Screen(int size) {
+            this.height = size;
+            this.width = size;
+        }
 
-    /// <summary>
-    /// Constructeur de la classe, crée une fenêtre rectangulaire
-    /// </summary>
-    /// <param name="width">Permet de définir la longueur de l'écran</param>
-    /// <param name="height">Permet de définir la largeur de l'écran</param>
-    public Screen(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
+        /// <summary>
+        /// Constructeur de la classe, crée une fenêtre rectangulaire
+        /// </summary>
+        /// <param name="width">Permet de définir la longueur de l'écran</param>
+        /// <param name="height">Permet de définir la largeur de l'écran</param>
+        public Screen(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
 
 
-    /* OLD WAY
-    /// <summary>
-    /// Permet d'afficher l'écran dans la console
-    /// </summary>
-    public void Display() {
-        List<string> lines = BuildBorder(); // on crée les bord de l'écran
-        var layers = this.layers.OrderBy(x => x.Key).Select(x => x.Value).ToList(); // on trie les éléments à afficher en fonct
-        foreach (var elements in layers)
-        {
-            foreach (KeyValuePair<Coordinates, string[]> element in elements) 
+        /* OLD WAY
+        /// <summary>
+        /// Permet d'afficher l'écran dans la console
+        /// </summary>
+        public void Display() {
+            List<string> lines = BuildBorder(); // on crée les bord de l'écran
+            var layers = this.layers.OrderBy(x => x.Key).Select(x => x.Value).ToList(); // on trie les éléments à afficher en fonct
+            foreach (var elements in layers)
             {
-                int index = element.Key.x + 1;
-                for (int i = 0; i < element.Value.Count(); i++)
+                foreach (KeyValuePair<Coordinates, string[]> element in elements) 
                 {
-                    if (index + element.Value[i].Length > this.width)
+                    int index = element.Key.x + 1;
+                    for (int i = 0; i < element.Value.Count(); i++)
                     {
-                        string celuiQuiDepassePas = element.Value[i].Substring(0, (this.width - index) -1);
-                        lines[element.Key.y + 1 + i] = lines[element.Key.y + 1].Remove(index, celuiQuiDepassePas.Length)
-                                                                            .Insert(index, celuiQuiDepassePas);
+                        if (index + element.Value[i].Length > this.width)
+                        {
+                            string celuiQuiDepassePas = element.Value[i].Substring(0, (this.width - index) -1);
+                            lines[element.Key.y + 1 + i] = lines[element.Key.y + 1].Remove(index, celuiQuiDepassePas.Length)
+                                                                                .Insert(index, celuiQuiDepassePas);
 
-                        Queue<string> file = new();
-                        file.Enqueue(element.Value[i].Substring(celuiQuiDepassePas.Length, element.Value[i].Length - celuiQuiDepassePas.Length));
-                        int lineIndex = 1;
-                        while(file.Count() != 0) {
-                            string elmt = file.Dequeue();
-                            if (elmt.Length > this.width)
-                            {
-                                Console.WriteLine(element.Value[i]);
-                                celuiQuiDepassePas = elmt.Substring(0, this.width - 2);
-                                lines[element.Key.y + 1 + lineIndex] = lines[element.Key.y + 1 + lineIndex]
-                                                                            .Remove(1, celuiQuiDepassePas.Length)
-                                                                            .Insert(1, celuiQuiDepassePas);
+                            Queue<string> file = new();
+                            file.Enqueue(element.Value[i].Substring(celuiQuiDepassePas.Length, element.Value[i].Length - celuiQuiDepassePas.Length));
+                            int lineIndex = 1;
+                            while(file.Count() != 0) {
+                                string elmt = file.Dequeue();
+                                if (elmt.Length > this.width)
+                                {
+                                    Console.WriteLine(element.Value[i]);
+                                    celuiQuiDepassePas = elmt.Substring(0, this.width - 2);
+                                    lines[element.Key.y + 1 + lineIndex] = lines[element.Key.y + 1 + lineIndex]
+                                                                                .Remove(1, celuiQuiDepassePas.Length)
+                                                                                .Insert(1, celuiQuiDepassePas);
 
-                                file.Enqueue(elmt.Substring(celuiQuiDepassePas.Length, elmt.Length - celuiQuiDepassePas.Length));
-                            } else
-                                lines[element.Key.y + 1 + lineIndex] = lines[element.Key.y + 1 + lineIndex].Remove(1, elmt.Length)
-                                                                                                            .Insert(1, elmt);
-                            lineIndex++;
+                                    file.Enqueue(elmt.Substring(celuiQuiDepassePas.Length, elmt.Length - celuiQuiDepassePas.Length));
+                                } else
+                                    lines[element.Key.y + 1 + lineIndex] = lines[element.Key.y + 1 + lineIndex].Remove(1, elmt.Length)
+                                                                                                                .Insert(1, elmt);
+                                lineIndex++;
+                            }
+
+                        } else{
+                            lines[element.Key.y + 1 + i] = lines[element.Key.y + 1].Remove(index, element.Value[i].Length)
+                                                                                .Insert(index, element.Value[i]);
                         }
-
-                    } else{
-                        lines[element.Key.y + 1 + i] = lines[element.Key.y + 1].Remove(index, element.Value[i].Length)
-                                                                               .Insert(index, element.Value[i]);
                     }
                 }
             }
+            Console.Write(string.Join("", lines));
+            Console.SetCursorPosition(0, 0);
         }
-        Console.Write(string.Join("", lines));
-        Console.SetCursorPosition(0, 0);
-    }
-    */
+        */
 
-    public void Display()
-    {
-        Console.Clear();
-        string background = string.Join("", BuildBorder()); // on crée les bord de l'écran
-        Console.Write(background);
-        foreach (List<Element> layer in layers.Values)
+        public void Display()
         {
-            foreach (Element element in layer)
+            Console.Clear();
+            string background = string.Join("", BuildBorder()); // on crée les bord de l'écran
+            Console.Write(background);
+
+            bool recall = false;
+
+            foreach (List<Element> layer in layers.Values)
             {
-                foreach (string text in element.text)
+                foreach (Element element in layer)
                 {
-                    if (element.placement == Placement.topLeft)
-                        Console.SetCursorPosition(element.coordinates.x, element.coordinates.y);
 
-                    else if (element.placement == Placement.mid)
-                        Console.SetCursorPosition(element.coordinates.x - (text.Length/2), element.coordinates.y);
 
-                    else if (element.placement == Placement.topRight)
-                        Console.SetCursorPosition(element.coordinates.x - text.Length, element.coordinates.y);
-                    
-                    else if (element.placement == Placement.botLeft)
-                        Console.SetCursorPosition(element.coordinates.x, element.coordinates.y - element.text.Length);
+                    for (int i = 0; i < element.text.Count(); i++)
+                    {
 
-                    else if (element.placement == Placement.botLeft)
-                        Console.SetCursorPosition(element.coordinates.x - text.Length, element.coordinates.y - element.text.Length);
+                        if (element.animation != Animation.None && element.animationIndex[i] > 0)
+                        {
+                            element.animationIndex[i]--;
+                            recall = true;
+                        }
 
-                    Console.ForegroundColor = element.foreground;
-                    Console.BackgroundColor = element.background;
-                    Console.Write(text);
+                        if (element.placement == Placement.topLeft)
+                            Console.SetCursorPosition(element.coordinates.x, element.coordinates.y + i);
+
+                        else if (element.placement == Placement.mid)
+                            Console.SetCursorPosition(element.coordinates.x - (element.text[i].Length/2), element.coordinates.y + i);
+
+                        else if (element.placement == Placement.topRight)
+                            Console.SetCursorPosition(element.coordinates.x - element.text[i].Length, element.coordinates.y + i);
+                        
+                        else if (element.placement == Placement.botLeft)
+                            Console.SetCursorPosition(element.coordinates.x, (element.coordinates.y+i) - element.text[i].Length);
+
+                        else if (element.placement == Placement.botLeft)
+                            Console.SetCursorPosition(element.coordinates.x - element.text[i].Length, (element.coordinates.y+i) - element.text[i].Length);
+
+                        Console.ForegroundColor = element.foreground;
+                        Console.BackgroundColor = element.background;
+
+                        if (element.animationIndex[i] >= element.text[i].Length) element.animationIndex[i] = -1;
+
+                        if (element.animationIndex[i] != -1)
+                            Console.Write(string.Join("", element.text[i].Take(element.text[i].Length - element.animationIndex[i])));
+                        else Console.Write(element.text[i]);
+                    }
+                    foreach (string text in element.text)
+                    {
+                        
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
                 }
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            Console.SetCursorPosition(0, 0);
+            if (recall) 
+            {
+                Thread.Sleep(200);
+                Display();
             }
         }
-        Console.SetCursorPosition(0, 0);
-    }
 
-    public void Add(Element element) {
-        this.Add(element, 0);
-    }
-
-    public void Add(Element element, int layer) {
-       if (!layers.ContainsKey(layer))
-            layers.Add(layer, new List<Element>());
-        layers[layer].Add(element);
-    }
-
-    /* TODO
-    public void Delete(Coordinates coordinates) {
-        this.Delete(coordinates, 0);
-    }
-
-    public void Delete(Coordinates coordinates, int layer) {
-        if(this.layers[layer].ContainsKey(coordinates))
-            this.layers[layer].Remove(coordinates);
-    }*/
-
-    public void DeleteLayer(int layer) {
-        if (this.layers.ContainsKey(layer)) {
-            this.layers.Remove(layer);
+        public void Add(Element element) {
+            this.Add(element, 0);
         }
-    }
 
-    public void Clear() {
-        this.layers.Clear();
-    }
+        public void Add(Element element, int layer) {
+        if (!layers.ContainsKey(layer))
+                layers.Add(layer, new List<Element>());
+            layers[layer].Add(element);
+        }
 
-    private List<string> BuildBorder() {
-        string top = "+" + new String('-', this.width-2) + "+\n";
-        string mid = "|" + new String(' ', this.width-2) + "|\n";
-        List<string> lines = Enumerable.Repeat(mid, this.height-2).ToList();
-        lines.Insert(0, top);
-        lines.Add(top);
-        return lines;
+        /* TODO
+        public void Delete(Coordinates coordinates) {
+            this.Delete(coordinates, 0);
+        }
+
+        public void Delete(Coordinates coordinates, int layer) {
+            if(this.layers[layer].ContainsKey(coordinates))
+                this.layers[layer].Remove(coordinates);
+        }*/
+
+        public void DeleteLayer(int layer) {
+            if (this.layers.ContainsKey(layer)) {
+                this.layers.Remove(layer);
+            }
+        }
+
+        public void Clear() {
+            this.layers.Clear();
+        }
+
+        private List<string> BuildBorder() {
+            string top = "+" + new String('-', this.width-2) + "+\n";
+            string mid = "|" + new String(' ', this.width-2) + "|\n";
+            List<string> lines = Enumerable.Repeat(mid, this.height-2).ToList();
+            lines.Insert(0, top);
+            lines.Add(top);
+            return lines;
+        }
     }
 }
